9958de7fa7b308f6cc4b0c9335f0932a68359006
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-01 14:53:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.lang import Builder
+import requests
 
 GUI = Builder.load_file('tela.kv')
 
@@ -9,7 +10,16 @@ class Meuaplicativo(App):
         return GUI
 
     def on_start(self):
-        self.root.ids
+        self.pegar_cota('USD')
+        self.root.ids['moeda1'].text = 'USD/BRL = R$7.07'
+        self.root.ids['moeda2'].text = 'EUR/BRL = RS$5.50'
+
+    def pegar_cota(self, moeda):
+        link = f'https://economia.awesomeapi.com.br/last/{moeda}-BRL'
+        requisicao = requests.get(link)
+        print(requisicao.json())
+        dic_requisicao = requisicao.json()
+        cotacao = dic_requisicao[f'{moeda}BRL']['bid']
 
 
 Meuaplicativo().run()

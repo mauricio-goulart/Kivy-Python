72ded4cf99c7e1187dd56cd9e72cdfffc83514df
scarlet-project <mauricioazevedogou@gmail.com>
2023-02-22 23:17:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class Meuaplicativo(App):
         print(requisicao.json())
         dic_requisicao = requisicao.json()
         cotacao = dic_requisicao[f'{moeda}BRL']['bid']
-        return cotaca
+        return cotacao
 
 
 Meuaplicativo().run()
